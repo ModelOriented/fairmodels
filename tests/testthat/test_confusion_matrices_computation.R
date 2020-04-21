cc4f12de0fc11be4d7e4beeb9ee3f527b57183bf
jakwisn <jakwisn@gmail.com>
2020-04-22 00:22:00 +0200

    factor added

--- tests/testthat/test_confusion_matrices_computation.R
+++ tests/testthat/test_confusion_matrices_computation.R
@@ -1,6 +1,6 @@
 test_that("confusion_matrices_for_groups", {
   explainer_gbm$y
-  test_data <- data.frame(sex = c(rep( "male", 7 ), rep("female", 7)),
+  test_data <- data.frame(sex = as.factor(c(rep( "male", 7 ), rep("female", 7))),
                           target = c(1,1,1,0,0,0,0,  1,1,1,1,0,0,0),
                           probabilities = c(0.61,0.55,0.1,0.1,0.45,0.69,0.88,   0.4,0.9,0.8,0.13,0.98,0.1,0.7))
 
@@ -13,8 +13,8 @@ test_that("confusion_matrices_for_groups", {
   expect_equal(gm$female$tn, 1)
   expect_equal(gm$female$fp, 2)
   expect_equal(gm$female$fn, 2)
-  expect_equal(gm$male$tp, 1)
   expect_equal(gm$male$tn, 2)
+  expect_equal(gm$male$tp, 1)
   expect_equal(gm$male$fp, 2)
   expect_equal(gm$male$fn, 2)
 
