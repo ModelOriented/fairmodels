52bd9aa4891fa4fb7791febc041ae01ce9d942f9
jakwisn <jakwisn@gmail.com>
2020-04-28 21:31:52 +0200

    test fix

--- tests/testthat/test_heatmap.R
+++ tests/testthat/test_heatmap.R
@@ -6,15 +6,10 @@ test_that("Test heatmap",{
   # heatmap converts shape while number of explainers exceed number of metrics
   expect_equal(c(small_plot$labels$x,small_plot$labels$y),c(big_plot$labels$y,big_plot$labels$x) )
 
+  top_dendogram <- plot_heatmap(fobject)[[1]]
 
-  # scaling
-  plt <- plot_heatmap(fobject, scale = TRUE)[[3]]
-
-  expected_val_hat <- mean(na.omit(plt$data[plt$data$metric == "TPR_parity_loss",]$score))
-  S_hat     <- (sd(na.omit(plt$data[plt$data$metric == "TPR_parity_loss",]$score)))^2
-
-  expect_lt(abs(expected_val_hat) , 0.001)
-  expect_lt(abs(S_hat-1) , 0.001)
+  expect_equal(top_dendogram$labels$title, "Heatmap")
+  expect_equal(top_dendogram$labels$subtitle, "With dendograms")
 
 
 
