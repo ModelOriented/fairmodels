3a3e745b226c022304bed70f1d130dd8090c6dc9
jakwisn <jakwisn@gmail.com>
2020-05-08 19:04:55 +0200

    test change

--- tests/testthat/test_prints.R
+++ tests/testthat/test_prints.R
@@ -2,7 +2,7 @@ test_that("test prints",{
 
 
   fh <- fairness_heatmap(fobject)
-  expect_class(print(fh), "array")
+  expect_class(print(fh), "matrix")
 
   cm <- choose_metric(fobject)
   fo <- print(fobject)
