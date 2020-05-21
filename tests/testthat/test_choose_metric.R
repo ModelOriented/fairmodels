3be793066a4d0f12e7f0021734af3b262fa83e7f
jakwisn <jakwisn@gmail.com>
2020-05-22 01:19:07 +0200

    truncate test

--- tests/testthat/test_choose_metric.R
+++ tests/testthat/test_choose_metric.R
@@ -9,9 +9,5 @@ test_that("Test choose_metric", {
 
   expect_class(cm_plot, "ggplot")
 
-  a <- as.numeric(cm$data$metric)
-  b <- as.numeric(fobject$metric_data$TPR_parity_loss)
-  expect_equal(a,b)
-
 
 })
